d9a8e9d91806de3ad894a8a5594ab312893c4d6a
thanhhai <buithanhhai0499@gmail.com>
2024-11-09 16:23:57 +0700

    update moi 4:23

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const middlewares = jsonServer.defaults();
 server.use(middlewares);
 server.use("/api", router);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 server.listen(PORT, () => {
   console.log(`JSON Server is running on port ${PORT}`);
 });
